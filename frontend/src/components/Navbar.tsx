0f4a1aca405aeaa59c3306d99f0e009ae4e833e4
Kaoutar-Taki <takikaoutar01@gmail.com>
2025-02-04 17:12:29 +0100

    Add dropdown menu to Navbar for user navigation; include avatar and links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,8 +6,24 @@ import {
 } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import avatarImg from "../assets/avatar.png";
+import { useState } from "react";
+
+const navigation = [
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Orders", href: "/orders" },
+  { name: "Cart Page", href: "/cart" },
+  { name: "Check Out", href: "/checkout" },
+];
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const currentUser = true;
+
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
@@ -28,7 +44,45 @@ const Navbar = () => {
         </div>
         {/* right side */}
         <div className="relative flex items-center md:space-x-3 space-x-2">
-          <HiOutlineUser className="size-6" />
+          <div className="">
+            {currentUser ? (
+              <>
+                <button onClick={toggleDropdown}>
+                  <img
+                    src={avatarImg}
+                    alt=""
+                    className={`size-7 rounded-full ${
+                      currentUser ? "ring-2 ring-blue-500" : ""
+                    }`}
+                  />
+                </button>
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="py-2">
+                      {navigation.map((nav) => (
+                        <li
+                          key={nav.name}
+                          onClick={() => setIsDropdownOpen(false)}
+                        >
+                          <Link
+                            to={nav.href}
+                            className="block px-4 py-2 text-sm hover:bg-gray-100"
+                          >
+                            {nav.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </>
+            ) : (
+              <Link to="/login">
+                <HiOutlineUser className="size-6" />
+              </Link>
+            )}
+          </div>
+
           <button className="hidden sm:block">
             <HiOutlineHeart className="size-6" />
           </button>
