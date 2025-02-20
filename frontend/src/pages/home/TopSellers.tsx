703e05003dea21d8d1831c329e503b60801a07bb
Kaoutar-Taki <takikaoutar01@gmail.com>
2025-02-20 14:14:45 +0100

    Ajout d'un carrousel Swiper pour afficher les livres les plus vendus par catégorie dans le composant TopSellers

--- frontend/src/pages/home/TopSellers.tsx
+++ frontend/src/pages/home/TopSellers.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import BookCard from "./books/BookCard";
 import { Book } from "../../types/book";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const categories = ["All Categories", "Business", "Fiction", "Horror", "Adventure"];
 
@@ -31,7 +36,6 @@ const TopSellers: React.FC = () => {
           onChange={e => setSelectedCategory(e.target.value)}
           className="rounded-md border border-gray-300 bg-[#EAEAEA] px-4 py-2 focus:outline-none"
         >
-          <option value="">All Categories</option>
           {categories.map((category, index) => (
             <option key={index} value={category}>
               {category}
@@ -39,15 +43,43 @@ const TopSellers: React.FC = () => {
           ))}
         </select>
       </div>
-      {filteredBooks.length > 0 ? (
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-          {filteredBooks.map((book, index) => (
-            <BookCard key={index} book={book} />
-          ))}
-        </div>
-      ) : (
-        <p>No books found in this category.</p>
-      )}
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {filteredBooks.length > 0 ? (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {filteredBooks.map((book, index) => (
+              <SwiperSlide key={index}>
+                <BookCard book={book} />
+              </SwiperSlide>
+            ))}
+          </div>
+        ) : (
+          <p>No books found in this category.</p>
+        )}
+      </Swiper>
     </div>
   );
 };
